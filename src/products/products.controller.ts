83d9343ca2d0138a58a74641992223886f70a60d
Anderson E. Peñaloza C <designeromega@gmail.com>
2022-05-14 16:59:47 +0200

    ad logic for product rest endpoint

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import {
+  CreateProductsDto,
+  UpdateProductsDto,
+} from './products-dto.interface';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  // @Get()
+  // getProducts(
+  //   @Query('limit') limit = 100,
+  //   @Query('offset') offset = 0,
+  //   @Query('brand') brand: string,
+  // ) {
+  //   return this.productsService.findAll();
+  // }
+
+  @Get()
+  getProducts() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':productId')
+  getOne(
+    @Param('productId', ParseIntPipe) productId: number,
+  ) {
+    return this.productsService.findOne(productId);
+  }
+
+  @Post()
+  create(@Body() payload: CreateProductsDto) {
+    return this.productsService.create(payload);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateProductsDto,
+  ) {
+    return this.productsService.update(id, payload);
+  }
+
+  @Delete()
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.delete(id);
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Products } from './products.interface';
+import {
+  CreateProductsDto,
+  UpdateProductsDto,
+} from './products-dto.interface';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private counterId = 1;
+  private products: Products[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'Description 1',
+      price: 1000,
+      stock: 100,
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    const product = this.products.find(
+      (item: Products) => item.id === id,
+    );
+    if (!product) {
+      throw new NotFoundException(
+        `Product ${id} not Found`,
+      );
+    }
+    return product;
+  }
+
+  create(payload: CreateProductsDto) {
+    this.counterId++;
+    const newProduct: Products = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  update(id: number, payload: UpdateProductsDto) {
+    const productId = this.findIndex(id);
+    if (productId != 1) {
+      const product = this.findOne(id);
+      this.products[productId] = {
+        ...product,
+        ...payload,
+      };
+      return this.products[id];
+    }
+    return null;
+  }
+
+  delete(id: number) {
+    const productId = this.findIndex(id);
+    if (productId === -1) {
+      throw new NotFoundException(
+        `Product ${id} not Found`,
+      );
+    }
+    this.products.slice(productId, 1);
+  }
+
+  private findIndex(id: number) {
+    return this.products.findIndex(
+      (item) => item.id === id,
+    );
+  }
+}
