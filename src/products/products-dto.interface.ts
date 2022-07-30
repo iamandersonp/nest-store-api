c2c495c41c2eea828a136a69653f1fab855a6aa8
Anderson Peñaloza <designeromega@gmail.com>
2022-07-30 11:17:43 +0200

    add class-validator validations

--- src/products/products-dto.interface.ts
+++ src/products/products-dto.interface.ts
@@ -1,13 +1,34 @@
-export interface CreateProductsDto {
+import {
+  IsString,
+  IsNumber,
+  IsNotEmpty,
+  IsPositive,
+  IsUrl,
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+export class CreateProductsDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsPositive()
   readonly price: number;
+
+  @IsNumber()
+  @IsPositive()
   readonly stock: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
+  readonly image: string;
 }
 
-export interface UpdateProductsDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-}
+export class UpdateProductsDto extends PartialType(
+  CreateProductsDto,
+) {}
