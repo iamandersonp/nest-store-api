26fcd6d93d886dddc0ab6244258672269b5bfedb
Anderson Peñaloza <designeromega@gmail.com>
2022-08-07 11:01:26 +0200

    remove indivudual imports and change into modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CustomersController } from './customers/customers.controller';
-import { CustomersService } from './customers/customers.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { ProductsController } from './products/products.controller';
-import { ProductsService } from './products/products.service';
-import { CategoriesController } from './categories/categories.controller';
-import { CategoriesService } from './categories/categories.service';
-import { OrdersController } from './orders/orders.controller';
-import { OrdersService } from './orders/orders.service';
-import { BrandsController } from './brands/brands.controller';
-import { BrandsService } from './brands/brands.service';
+
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    AppController,
-    CustomersController,
-    UsersController,
-    ProductsController,
-    CategoriesController,
-    OrdersController,
-    BrandsController,
-  ],
-  providers: [
-    AppService,
-    CustomersService,
-    UsersService,
-    ProductsService,
-    CategoriesService,
-    OrdersService,
-    BrandsService,
-  ],
+  imports: [ProductsModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
