e24a47168aa7d6166d3617ea9e9ad10f439c4833
Anderson Peñaloza <designeromega@gmail.com>
2023-04-01 13:29:25 +0200

    add user documentation

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -14,25 +14,59 @@ import {
   UpdateUserDto,
 } from '../dtos/users-dto.interface';
 
+/**
+ * Users Controller
+ *
+ * @export
+ * @class UsersController
+ */
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  /**
+   * Get all users
+   *
+   * @return {*} {User[]}
+   * @memberof UsersController
+   */
   @Get()
   getProducts() {
     return this.usersService.findAll();
   }
 
+  /**
+   * Get one user
+   *
+   * @param {number} userId
+   * @return {*} {User}
+   * @memberof UsersController
+   */
   @Get(':userId')
   getOne(@Param('userId', ParseIntPipe) userId: number) {
     return this.usersService.findOne(userId);
   }
 
+  /**
+   * Create a new user
+   *
+   * @param {CreateUserDto} payload - User data
+   * @return {* } {User}
+   * @memberof UsersController
+   */
   @Post()
   create(@Body() payload: CreateUserDto) {
     return this.usersService.create(payload);
   }
 
+  /**
+   * Update a user
+   *
+   * @param {number} id - User id
+   * @param {UpdateUserDto} payload - User data
+   * @return {*} {User}
+   * @memberof UsersController
+   */
   @Put(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -41,6 +75,13 @@ export class UsersController {
     return this.usersService.update(id, payload);
   }
 
+  /**
+   * Delete a user
+   *
+   * @param {number} id - User id
+   * @return {*} {User}
+   * @memberof UsersController
+   */
   @Delete()
   delete(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.delete(id);
