f34995dd24edd066bdaa90b1b05aad0d84ed1297
Anderson Peñaloza <designeromega@gmail.com>
2023-04-01 15:39:16 +0200

    update documentation

--- src/users/models/users.interface.ts
+++ src/users/models/users.interface.ts
@@ -1,7 +1,55 @@
+/**
+ * Interface for Users
+ *
+ * @export
+ * @interface Users
+ */
 export interface Users {
+  /**
+   * Id of the user
+   *
+   * @type {number}
+   * @memberof Users
+   */
   id: number;
+
+  /**
+   * First name of the user
+   *
+   * @type {string}
+   * @memberof Users
+   */
   firstName: string;
+
+  /**
+   * Last name of the user
+   *
+   * @type {string}
+   * @memberof Users
+   */
   lastName: string;
+
+  /**
+   * UserName of the user
+   *
+   * @type {string}
+   * @memberof Users
+   */
   userName: string;
+
+  /**
+   * Password of the user
+   *
+   * @type {string}
+   * @memberof Users
+   */
   password: string;
+
+  /**
+   * Role of the user
+   *
+   * @type {string}
+   * @memberof Users
+   */
+  role: string;
 }
