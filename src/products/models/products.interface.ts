a0915bd37a4000489797d9273fc0aba18eef0269
Anderson Peñaloza <designeromega@gmail.com>
2023-04-01 14:05:22 +0200

    add documentation

--- src/products/models/products.interface.ts
+++ src/products/models/products.interface.ts
@@ -1,8 +1,50 @@
+/**
+ * Interface for the 'Products' data
+ *
+ * @export
+ * @interface Products
+ */
 export interface Products {
+  /**
+   * Product id
+   *
+   * @type {number}
+   * @memberof Products
+   */
   id: number;
+  /**
+   * Product name
+   *
+   * @type {string}
+   * @memberof Products
+   */
   name: string;
+  /**
+   * Product description
+   *
+   * @type {string}
+   * @memberof Products
+   */
   description: string;
+  /**
+   * Product price
+   *
+   * @type {number}
+   * @memberof Products
+   */
   price: number;
+  /**
+   * Product stock
+   *
+   * @type {number}
+   * @memberof Products
+   */
   stock: number;
+  /**
+   * Product image
+   *
+   * @type {string}
+   * @memberof Products
+   */
   image: string;
 }
