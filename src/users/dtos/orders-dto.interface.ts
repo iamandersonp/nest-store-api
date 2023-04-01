3607157a845b4dc725009dfd3ec731e9f4db9cc4
Anderson Peñaloza <designeromega@gmail.com>
2023-04-01 13:41:15 +0200

    change orders dto to class to add validation

--- src/users/dtos/orders-dto.interface.ts
+++ src/users/dtos/orders-dto.interface.ts
@@ -1,11 +1,49 @@
-export interface CreateOrdersDto {
+import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsNumber,
+  IsPositive,
+  IsDate,
+} from 'class-validator';
+
+export class CreateOrdersDto {
+  /**
+   * Customer ID
+   *
+   * @type {number}
+   * @memberof CreateOrdersDto
+   */
+  @IsNumber()
+  @IsPositive()
   readonly customerId: number;
+  /**
+   * Date
+   *
+   * @type {string}
+   * @memberof CreateOrdersDto
+   */
+  @IsDate()
   readonly date: string;
+
+  /**
+   *
+   *
+   * @type {number}
+   * @memberof CreateOrdersDto
+   */
+  @IsNumber()
+  @IsPositive()
   readonly total: number;
 }
 
-export interface UpdateOrdersDto {
-  readonly customerId?: number;
-  readonly date?: string;
-  readonly total?: number;
-}
+/**
+ * Update Orders DTO
+ *
+ * @export
+ * @class UpdateOrdersDto
+ * @extends {PartialType(
+ *   CreateOrdersDto,
+ * )}
+ */
+export class UpdateOrdersDto extends PartialType(
+  CreateOrdersDto,
+) {}
