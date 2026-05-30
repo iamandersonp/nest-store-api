e2953d0146d775ca716d96c0f0406829c538b747
Anderson E. Peñaloza C. <info@iamanderson.dev>
2026-05-30 23:28:51 +0100

    fix(users): customers controller should depend on use-case, not adapter port
    Signed-off-by: Anderson E. Peñaloza C. <info@iamanderson.dev>

--- src/users/infrastructure/adapters/in/v1/controllers/customers.controller.ts
+++ src/users/infrastructure/adapters/in/v1/controllers/customers.controller.ts
@@ -5,7 +5,6 @@ import {
   Get,
   HttpCode,
   HttpStatus,
-  Inject,
   Param,
   ParseIntPipe,
   Post,
@@ -13,7 +12,6 @@ import {
 } from '@nestjs/common';
 
 import { CustomerUseCaseService } from '@users/application/customer-use-case.service';
-import { CUSTOMERS_SERVICE_PORT } from '@users/domain/ports/customer.port';
 import type { Customer } from '../../../../../domain/models/customer.entity';
 import { CreateCustomerDto, UpdateCustomerDto } from '../dtos/customers.dto.';
 
@@ -22,10 +20,12 @@ import { CreateCustomerDto, UpdateCustomerDto } from '../dtos/customers.dto.';
   version: '1',
 })
 export class CustomersController {
-  constructor(
-    @Inject(CUSTOMERS_SERVICE_PORT)
-    private readonly service: CustomerUseCaseService,
-  ) {}
+  /**
+   * Creates an instance of CustomersController.
+   * @param {CustomerUseCaseService} service
+   * @memberof CustomersController
+   */
+  constructor(private readonly service: CustomerUseCaseService) {}
 
   /**
    * Get all Customers
